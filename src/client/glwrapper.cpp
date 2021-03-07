ce75620757d706541b267495c4fe3bec15849779
adamhutchings <adam.abahot@gmail.com>
2021-03-06 16:47:24 -0800

    Add OpenGL / GLFW wrapper

---
+++ src/client/glwrapper.cpp
@@ -0,0 +1,43 @@
+#include "glwrapper.h"
+
+namespace {
+
+    // The window handle
+    GLFWwindow* wn;
+
+}
+
+namespace cmgl {
+
+void init() {
+    glfwInit();
+}
+
+void cleanup() {
+    glfwTerminate();
+}
+
+void create_window() {
+#ifdef __APPLE__
+    glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#endif // __APPLE__
+    wn = glfwCreateWindow(500, 500, "Crinemaft", NULL, NULL);
+    glfwMakeContextCurrent(wn);
+}
+
+void update_window() {
+    while (!glfwWindowShouldClose(wn)) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glfwSwapBuffers(wn);
+        glfwPollEvents();
+    }
+}
+
+bool window_open() {
+    return !glfwWindowShouldClose(wn);
+}
+
+}  // namespace cmgl
---
+++ src/client/glwrapper.h
@@ -0,0 +1,33 @@
+/**
+ * OpenGL / GLFW wrapper - a uniform interface which should include all
+ * OS-specific rendering / windowing capabilities.
+ */
+
+#pragma once
+
+#pragma once
+
+/**
+ * OpenGL is deprecated on Apple.
+ */
+#ifdef __APPLE__
+    #define GL_SILENCE_DEPRECATION
+#endif // __APPLE__
+
+#define GLFW_INCLUDE_GLCOREARB
+
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
+
+// Crinemaft GL
+namespace cmgl {
+
+    void init();
+    void cleanup();
+
+    void create_window();
+    void update_window();
+
+    bool window_open();
+
+}
