a87ee9307de21d299331305f945bb9fef8bc4918
adamhutchings <adam.abahot@gmail.com>
2021-03-06 17:10:15 -0800

    Move window hints to init

--- src/client/glwrapper.cpp
+++ src/client/glwrapper.cpp
@@ -11,6 +11,12 @@ namespace cmgl {
 
 void init() {
     glfwInit();
+#ifdef __APPLE__
+    glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#endif // __APPLE__
 }
 
 void cleanup() {
@@ -18,12 +24,6 @@ void cleanup() {
 }
 
 void create_window() {
-#ifdef __APPLE__
-    glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#endif // __APPLE__
     wn = glfwCreateWindow(500, 500, "Crinemaft", NULL, NULL);
     glfwMakeContextCurrent(wn);
 }
