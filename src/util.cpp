c8d6c683eca9fbc467ba0f0425609e06851ed721
adamhutchings <adam.abahot@gmail.com>
2021-03-07 23:02:18 -0800

    Fix file read

--- src/util.cpp
+++ src/util.cpp
@@ -4,8 +4,9 @@ namespace util {
 
 std::string get_full_source(std::ifstream const& file) {
     std::stringstream buffer;
-    buffer << file.rdbuf();
-    return buffer.str();
+    buffer << file.rdbuf() << "\0";
+    std::string shaderCode = buffer.str().c_str();
+    return shaderCode;
 }
 
 }
